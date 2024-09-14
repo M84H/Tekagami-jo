61e07f57fb7394c9ee8851e7fdafd36d06170444
M84H <40142101+M84H@users.noreply.github.com>
2024-09-14 13:22:48 -0400

    added tekagami-jo files

---
+++ index.js
@@ -0,0 +1,33 @@
+import Mirador from 'mirador/dist/es/src/index';
+import examplePlugin from './plugins/example_plugin';
+
+const config = {
+  id: 'mirador-viewer',
+  translations: {
+    en: {
+      "currentItem_1/2": "Right",
+      "currentItem_2/2": "Left"
+    }
+
+  },
+  theme: { direction: 'rtl' },
+  selectedTheme: "dark",
+  window: {
+    sideBarPanel: "info",
+    defaultView:'book',
+      views: [
+        {key:"single",behaviors:["individuals","paged"]}, 
+        {key:"book",behaviors:["individuals"]}, 
+        {key:"gallery"} 
+      ],
+  },
+  windows: [
+    {
+      manifestId: 'http://localhost:8080/2666.json',
+    }
+  ],
+};
+
+const plugins = [];
+
+Mirador.viewer(config, plugins);
